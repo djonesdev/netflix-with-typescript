f64ccc4ed9b35bfd22b603cb6991a2cfec5834b2
djonesdev <domajones@outlook.com>
2023-08-09 18:33:43 +0100

    fix: TICKET-0000: add more sementic html to login form

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,4 +1,5 @@
 import { useState, useCallback } from "react";
+import Image from "next/image";
 
 import Input from "@/components/Input";
 
@@ -7,14 +8,20 @@ export default function Auth() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const [variant, setVariant] = useState("login");
+  const [variant, setVariant] = useState<string>("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant: string) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
 
   const login = useCallback(async () => {
-    console.log('login')
+    console.log("login");
   }, [email, password]);
 
   const register = useCallback(async () => {
-    console.log('register')
+    console.log("register");
   }, [email, name, password, login]);
 
   return (
@@ -23,17 +30,17 @@ export default function Auth() {
         <nav className="px-12 py-5">
           <img src="/images/logo.png" alt="Netflix logo" className="h-12" />
         </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/3 lg:max-width-md rounded-md w-full">
+        <main className="flex justify-center">
+          <section className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/3 lg:max-width-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Sign In</h2>
-            <div className="flex flex-col gap-4">
-            {variant === 'register' && (
+            <form className="flex flex-col gap-4">
+              {variant === "register" && (
                 <Input
                   id="name"
                   type="text"
                   label="Username"
                   value={name}
-                  onChange={(e: any) => setName(e.target.value)} 
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
               <Input
@@ -41,27 +48,36 @@ export default function Auth() {
                 type="email"
                 label="Email address or phone number"
                 value={email}
-                onChange={(e: any) => setEmail(e.target.value)} 
+                onChange={(e: any) => setEmail(e.target.value)}
               />
               <Input
-                type="password" 
-                id="password" 
-                label="Password" 
+                type="password"
+                id="password"
+                label="Password"
                 value={password}
-                onChange={(e: any) => setPassword(e.target.value)} 
+                onChange={(e: any) => setPassword(e.target.value)}
               />
-            </div>
-            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
-              {variant === 'login' ? 'Login' : 'Sign up'}
-            </button>
+              <button
+                type="submit"
+                value="Submit"
+                aria-label={variant}
+                onClick={variant === "login" ? login : register}
+                className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
+              >
+                {variant === "login" ? "Login" : "Sign up"}
+              </button>
+            </form>
             <p className="text-neutral-500 mt-12">
               First time using Netflix?
-              <span className="text-white ml-1 hover:underline cursor-pointer">
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
                 Create an account
               </span>
             </p>
-          </div>
-        </div>
+          </section>
+        </main>
       </div>
     </div>
   );
