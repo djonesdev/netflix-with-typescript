72de38d9148660284d10e05ee9057b33e577380a
djonesdev <domajones@outlook.com>
2023-09-06 11:43:10 +0100

    f/0000/get logged in user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,35 @@
+import { NextPageContext } from "next/types";
+import { signOut, getSession } from "next-auth/react";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
+  console.log(user, 'this is the user?')
   return (
-    <h2>Netflix with typescript</h2>
-  )
+    <>
+      <h2>Netflix with typescript</h2>
+      <p className="text-white">Logged in as: {user?.name}</p>
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        signOut
+      </button>
+    </>
+  );
 }
